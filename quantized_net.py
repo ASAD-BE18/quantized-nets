9deb3924bddbae06b2d0d986bc4dbac8158eb5be
Asad Majeed <80450984+ASAD-BE18@users.noreply.github.com>
2024-05-23 23:33:17 +0500

    Update quantized_net.py

--- quantized_net.py
+++ quantized_net.py
@@ -22,10 +22,10 @@ from quantize.quantized_ops import quantized_tanh as quantized_tanh_op
 import math
 from argparse import ArgumentParser
 import tensorflow as tf
+
 from keras.callbacks import EarlyStopping
 
-# Define early stopping
-early_stopping = EarlyStopping(monitor='val_loss', patience=10)
+early_stopping = EarlyStopping(monitor='val_loss', patience=10, verbose=1)
 
 
 from keras.datasets import cifar10, fashion_mnist, mnist
@@ -96,10 +96,6 @@ class TestCallback(Callback):
         print('\nTesting loss: {}, acc: {}\n'.format(loss, acc))
 
 
-
-H = 1.
-kernel_lr_multiplier = 'Glorot'
-
 # nn
 batch_size = 128
 epochs = 1000 
@@ -118,67 +114,75 @@ lr_start = 1e-3
 lr_end = 1e-4
 lr_decay = (lr_end / lr_start)**(1. / epochs)
 
-# BN
-epsilon = 1e-6
-momentum = 0.9
 
-def add_quant_conv_layer(model, conv_num_filters, conv_kernel_size, conv_strides, mpool_kernel_size, mpool_strides, n_bits):
+def create_model(img_rows, img_cols):
 
-    model.add(QuantizedConv2D(conv_num_filters, kernel_size=(conv_kernel_size,conv_kernel_size), input_shape=( img_rows, img_cols, channels),
-                           data_format='channels_last', strides=(conv_strides,conv_strides),
-                           H=H, kernel_lr_multiplier=kernel_lr_multiplier, 
-                           padding='valid', use_bias=use_bias, nb = n_bits))
-    model.add(MaxPooling2D(pool_size=(mpool_kernel_size, mpool_kernel_size),strides = (mpool_strides,mpool_strides) ,padding='valid' , data_format='channels_last'))
-    model.add(BatchNormalization(epsilon=epsilon, momentum=momentum, axis=1))
+    H = 1.
+    kernel_lr_multiplier = 'Glorot'
+
+
+    # BN
+    epsilon = 1e-6
+    momentum = 0.9
+
+    def add_quant_conv_layer(model, conv_num_filters, conv_kernel_size, conv_strides, mpool_kernel_size, mpool_strides, n_bits):
+
+        model.add(QuantizedConv2D(conv_num_filters, kernel_size=(conv_kernel_size,conv_kernel_size), input_shape=( img_rows, img_cols, channels),
+                            data_format='channels_last', strides=(conv_strides,conv_strides),
+                            H=H, kernel_lr_multiplier=kernel_lr_multiplier, 
+                            padding='valid', use_bias=use_bias, nb = n_bits))
+        model.add(MaxPooling2D(pool_size=(mpool_kernel_size, mpool_kernel_size),strides = (mpool_strides,mpool_strides) ,padding='valid' , data_format='channels_last'))
+        model.add(BatchNormalization(epsilon=epsilon, momentum=momentum, axis=1))
+        model.add(Activation(partial(quantized_relu_op, nb = n_bits)))
+        return model 
+
+
+    assert n_bits >= 2 , "Numer of bits should be at least 2 and atmost 32"
+    assert n_bits <= 32 , "Numer of bits should be at least 2 and atmost 32"
+
+    print("Quantize to bits: ", n_bits)
+    print(type(n_bits))
+
+    # -------------Model Architecture-----
+
+    model = Sequential()
+
+    conv_kernel_size = 5
+    conv_num_filters = 32
+    conv_strides = 2
+    mpool_kernel_size = 2
+    mpool_strides = 2
+
+    add_quant_conv_layer(model = model, conv_num_filters = conv_num_filters, conv_kernel_size = conv_kernel_size, conv_strides = conv_strides, mpool_kernel_size = mpool_kernel_size, mpool_strides = mpool_strides, n_bits=n_bits)
+
+
+    conv_kernel_size = 3
+    conv_num_filters = 64
+    conv_strides = 1
+    mpool_kernel_size = 2
+    mpool_strides = 2
+
+    add_quant_conv_layer(model = model, conv_num_filters = conv_num_filters, conv_kernel_size = conv_kernel_size, conv_strides = conv_strides, mpool_kernel_size = mpool_kernel_size, mpool_strides = mpool_strides, n_bits=n_bits)
+
+    model.add(Flatten())
+
+    # dense1
+    model.add(QuantizedDense(512, H=H, kernel_lr_multiplier=kernel_lr_multiplier, use_bias=use_bias, nb = n_bits))
+    model.add(BatchNormalization(epsilon=epsilon, momentum=momentum ))
     model.add(Activation(partial(quantized_relu_op, nb = n_bits)))
-    return model 
+    # dense2
+    model.add(QuantizedDense(classes, H=H, kernel_lr_multiplier=kernel_lr_multiplier, use_bias=use_bias, nb = n_bits))
+    model.add(BatchNormalization(epsilon=epsilon, momentum=momentum, name='bn6'))
 
-
-assert n_bits >= 2 , "Numer of bits should be at least 2 and atmost 32"
-assert n_bits <= 32 , "Numer of bits should be at least 2 and atmost 32"
-
-print("Quantize to bits: ", n_bits)
-print(type(n_bits))
-
-# -------------Model Architecture-----
-
-model = Sequential()
-
-conv_kernel_size = 5
-conv_num_filters = 32
-conv_strides = 2
-mpool_kernel_size = 2
-mpool_strides = 2
-
-add_quant_conv_layer(model = model, conv_num_filters = conv_num_filters, conv_kernel_size = conv_kernel_size, conv_strides = conv_strides, mpool_kernel_size = mpool_kernel_size, mpool_strides = mpool_strides, n_bits=n_bits)
-
-
-conv_kernel_size = 3
-conv_num_filters = 64
-conv_strides = 1
-mpool_kernel_size = 2
-mpool_strides = 2
-
-add_quant_conv_layer(model = model, conv_num_filters = conv_num_filters, conv_kernel_size = conv_kernel_size, conv_strides = conv_strides, mpool_kernel_size = mpool_kernel_size, mpool_strides = mpool_strides, n_bits=n_bits)
-
-model.add(Flatten())
-
-# dense1
-model.add(QuantizedDense(512, H=H, kernel_lr_multiplier=kernel_lr_multiplier, use_bias=use_bias, nb = n_bits))
-model.add(BatchNormalization(epsilon=epsilon, momentum=momentum ))
-model.add(Activation(partial(quantized_relu_op, nb = n_bits)))
-# dense2
-model.add(QuantizedDense(classes, H=H, kernel_lr_multiplier=kernel_lr_multiplier, use_bias=use_bias, nb = n_bits))
-model.add(BatchNormalization(epsilon=epsilon, momentum=momentum, name='bn6'))
-
-opt = Adam(lr=lr_start) 
-model.compile(loss='squared_hinge', optimizer=opt, metrics=[
-        'acc',
-        tf.keras.metrics.Precision(name='precision'),
-        tf.keras.metrics.Recall(name='recall'),
-        tf.keras.metrics.AUC(curve='PR', name='auc_pr')  # AUC of the PR curve is equivalent to the F1 score
-    ])
-model.summary()
+    opt = Adam(lr=lr_start) 
+    model.compile(loss='squared_hinge', optimizer=opt, metrics=[
+            'acc',
+            tf.keras.metrics.Precision(name='precision'),
+            tf.keras.metrics.Recall(name='recall'),
+            tf.keras.metrics.AUC(curve='PR', name='auc_pr')  # AUC of the PR curve is equivalent to the F1 score
+        ])
+    model.summary()
+    return model
 
 # ---------------------------------
 
@@ -203,6 +207,7 @@ print("Y train: ", y_train.shape)
 
 # -------- Train Loop----------------------
 
+model = create_model(28, 28)
 lr_scheduler = LearningRateScheduler(lambda e: lr_start * lr_decay ** e)
 history = model.fit(x_train, y_train,
                     batch_size=batch_size, epochs=epochs,
@@ -228,6 +233,12 @@ test_labels = y_test_cifar10
 
 x = [train_data, train_labels, test_data, test_labels]
 x_train, y_train, x_test, y_test = cifar10_process(x)
+# remove the the last 3
+x_train = x_train[:,:,:,0]
+x_test = x_test[:,:,:,0]
+x_train = np.reshape(x_train, [-1, 32, 32, 1])
+x_test = np.reshape(x_test, [-1, 32, 32, 1])
+
 
 print("X train: ", x_train.shape)
 print("Y train: ", y_train.shape)
@@ -236,6 +247,7 @@ print("Y train: ", y_train.shape)
 
 # -------- Train Loop----------------------
 
+model = create_model(32, 32)
 lr_scheduler = LearningRateScheduler(lambda e: lr_start * lr_decay ** e)
 history = model.fit(x_train, y_train,
                     batch_size=batch_size, epochs=epochs,
@@ -268,6 +280,7 @@ print("Y train: ", y_train.shape)
 
 # -------- Train Loop----------------------
 
+model = create_model(28, 28)
 lr_scheduler = LearningRateScheduler(lambda e: lr_start * lr_decay ** e)
 history = model.fit(x_train, y_train,
                     batch_size=batch_size, epochs=epochs,
@@ -283,3 +296,4 @@ print('Fashion MNIST Test precision:', score[2])
 print('Fashion MNIST Test recall:', score[3])
 print('Fashion MNIST Test F1:', score[4])
 # ---------------------------------------
+
